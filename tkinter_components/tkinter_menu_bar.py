869290e75ae3b93622293f92db79b033663e3a87
EmirhanDoke <sed.emirhan.doke@gmail.com>
2025-05-11 16:36:03 +0300

    update menu_bar

--- tkinter_components/tkinter_menu_bar.py
+++ tkinter_components/tkinter_menu_bar.py
@@ -11,7 +11,7 @@ class menu_bar():
     def __init__(self, root):
         
         self.root = root
-        self.settings_file = "user_settings.txt"  # Ayarların kaydedileceği dosya
+        self.settings_file = "user_settings.txt"  # Path to the settings file
         self.show_image_flag = tk.BooleanVar()
         self.language_select = tk.StringVar()
         self.process_position = tk.StringVar()
@@ -19,30 +19,29 @@ class menu_bar():
         self.load_user_settings()
         
         
-        menu_cubugu = tk.Menu(self.root)
+        menu_line = tk.Menu(self.root)
         
         # Settings Menu
-        settings_menu = tk.Menu(menu_cubugu, tearoff=0)
+        settings_menu = tk.Menu(menu_line, tearoff=0)
         settings_menu.add_command(label="Settings", command=self.open_settings_window)
-        # Ana menüye "Settings" menüsünü ekle
-        menu_cubugu.add_cascade(label="Settings", menu=settings_menu, font = ("Ariel", 12))
         
-        restart_menu = tk.Menu(menu_cubugu, tearoff=0)
+        menu_line.add_cascade(label="Settings", menu=settings_menu, font = ("Ariel", 12))
+        
+        restart_menu = tk.Menu(menu_line, tearoff=0)
         restart_menu.add_command(label="Are you sure for restart?", command=self.restart_app)
-        menu_cubugu.add_cascade(label="Restart", menu=restart_menu, font = ("Ariel", 12))
+        menu_line.add_cascade(label="Restart", menu=restart_menu, font = ("Ariel", 12))
         
-        tools_menu = tk.Menu(menu_cubugu, tearoff=0)
+        tools_menu = tk.Menu(menu_line, tearoff=0)
         tools_menu.add_command(label="Tool 1")
-        menu_cubugu.add_cascade(label="Tools", menu=tools_menu, font = ("Ariel", 12))
+        menu_line.add_cascade(label="Tools", menu=tools_menu, font = ("Ariel", 12))
         
         # Info Menu
-        About_menu = tk.Menu(menu_cubugu, tearoff=0)
-        About_menu.add_command(label="About")
-        # Ana menüye "About" menüsünü ekle
-        menu_cubugu.add_cascade(label="About", menu=About_menu, font = ("Ariel", 12))
-        
+        about_menu = tk.Menu(menu_line, tearoff=0)
+        about_menu.add_command(label="About")
+        # Add the "About" menu to the main menu
+        menu_line.add_cascade(label="About", menu=about_menu, font = ("Ariel", 12))
 
-        root.config(menu=menu_cubugu)
+        root.config(menu=menu_line)
         
     def open_settings_window(self):
         settings_win = tk.Toplevel(self.root)
@@ -70,7 +69,7 @@ class menu_bar():
         process_position_frame = tk.LabelFrame(settings_win, text="Process Position", font=("Arial", 9), borderwidth=2, relief="solid")
         process_position_frame.pack(side = "top", padx=2, pady=10, fill=tk.X)
         
-        tk.Label(process_position_frame, text="Height", font=("Arial", 10)).grid(row=0, column=0, padx=5, pady=5)
+        tk.Label(process_position_frame, text="Height", font=("Arial", 10)).grid(row=0, column=0, padx=0, pady=5)
         process_position_entry = tk.Entry(process_position_frame, font=("Arial", 10), textvariable=self.process_position, width=10)
         process_position_entry.grid(row=0, column=1, padx=5, pady=5)
     
